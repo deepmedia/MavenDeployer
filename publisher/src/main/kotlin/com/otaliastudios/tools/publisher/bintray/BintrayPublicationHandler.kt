1a381e4149506d7a743be185afca6a537c024fb7
Mattia Iavarone <mat.iavarone@gmail.com>
2020-05-28 11:16:16 -0300

    Support multiple bintray publications at once

--- publisher/src/main/kotlin/com/otaliastudios/tools/publisher/bintray/BintrayPublicationHandler.kt
+++ publisher/src/main/kotlin/com/otaliastudios/tools/publisher/bintray/BintrayPublicationHandler.kt
@@ -1,9 +1,12 @@
 package com.otaliastudios.tools.publisher.bintray
 
 import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
+import com.jfrog.bintray.gradle.tasks.BintrayPublishTask
 import com.otaliastudios.tools.publisher.Publication
 import com.otaliastudios.tools.publisher.PublicationHandler
 import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.plugins.BasePluginConvention
 import org.gradle.kotlin.dsl.delegateClosureOf
 import java.util.*
@@ -48,29 +51,51 @@ internal class BintrayPublicationHandler : PublicationHandler() {
         base.archivesBaseName = publication.project.artifact!!
 
         // Configure the plugin with the publication data.
-        val bintray = target.extensions.getByType(BintrayExtension::class.java)
+        // We're replicating what ProjectsEvaluatedBuildListener.groovy does in the BGP
+        val bintray = target.tasks.create("bintrayUpload${mavenPublication.capitalize()}", BintrayUploadTask::class.java)
+        bintray.project = target
         bintray.setPublications(mavenPublication)
+        bintray.apiUrl = "https://api.bintray.com" // BintrayUploadTask.API_URL_DEFAULT
         bintray.user = publication.auth.user ?: ""
-        bintray.key = publication.auth.key ?: ""
+        bintray.apiKey = publication.auth.key ?: ""
         bintray.override = true
         bintray.publish = true
         bintray.dryRun = publication.dryRun
-        bintray.pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-            repo = publication.auth.repo ?: ""
-            name = publication.project.name
-            publication.project.description?.let { desc = it }
-            publication.project.vcsUrl?.let { vcsUrl = it }
-            val licenses = publication.project.licenses
-            if (licenses.isNotEmpty()) {
-                setLicenses(*licenses.map { it.name }.toTypedArray())
+        bintray.repoName = publication.auth.repo ?: ""
+        bintray.packageName = publication.project.name
+        publication.project.description?.let { bintray.packageDesc = it }
+        publication.project.vcsUrl?.let { bintray.packageVcsUrl = it }
+        val licenses = publication.project.licenses
+        if (licenses.isNotEmpty()) {
+            bintray.setPackageLicenses(*licenses.map { it.name }.toTypedArray())
+        }
+        bintray.versionName = publication.release.version!!
+        bintray.versionDesc = publication.release.description!!
+        bintray.versionReleased = Date().toString()
+        bintray.versionVcsTag = publication.release.vcsTag!!
+        bintray.dependsOn("publish${mavenPublication.capitalize()}PublicationToMavenLocal")
+
+        // Need to call BintrayPublishTask.publishVersion to mark the version as published.
+        // This is the actual bintrayPublish task that we'd call in the normal extension flow.
+        val bintrayPublish = target.tasks.create("bintrayPublish${mavenPublication.capitalize()}", BintrayPublishTask::class.java)
+        bintray.doLast {
+            if (didWork) {
+                bintrayPublishMethod.invoke(bintrayPublish,
+                    bintray.repoName, bintray.packageName, bintray.versionName, bintray)
             }
-            version(delegateClosureOf<BintrayExtension.VersionConfig> {
-                name = publication.release.version!!
-                desc = publication.release.description!!
-                released = Date().toString()
-                vcsTag = publication.release.vcsTag!!
-            })
-        })
-        return setOf("bintrayUpload")
+        }
+
+        return setOf(bintray.name)
+    }
+
+
+    private val bintrayPublishMethod by lazy {
+        BintrayPublishTask::class.java.getDeclaredMethod("publishVersion",
+            String::class.java,
+            String::class.java,
+            String::class.java,
+            BintrayUploadTask::class.java).also {
+            it.isAccessible = true
+        }
     }
 }
